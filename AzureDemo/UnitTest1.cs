c5d789bdb7a3ec67eb16090dc6ef28b1fed4047e
Sukumar Kachakayala <sukumar.kachakayala@zenq.com>
2018-11-02 18:48:13 +0530

    test

--- AzureDemo/UnitTest1.cs
+++ AzureDemo/UnitTest1.cs
@@ -1,20 +1,29 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
 
 namespace AzureDemo
 {
     [TestClass]
     public class UnitTest1
     {
-        
+        IWebDriver driver;
         [TestMethod]
         public void TestMethod1()
         {
-           
+            launchBrowser();
             Console.WriteLine("chrome browser lunched successfully");
 
         }
-        
+        public void launchBrowser()
+        {
+            driver = new ChromeDriver(@"D:\selenium jars");
+            //driver.Manage().Timeouts().ImplicitWait = 20;
+            driver.Manage().Window.Maximize();
+            driver.Url = "https://docs.microsoft.com/en-us/azure/devops/test/run-automated-tests-from-test-hub?view=vsts";
+        }
+
     }
 }
 
